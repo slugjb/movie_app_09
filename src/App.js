798d8317d6c950780eb95504534b84411b99188a
Junbae Park <slug_jb@naver.com>
2021-09-15 17:17:10 +0900

    0915_v01.1

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from "react";
 
-function Food({fav})
+function Food({name, picture})
 {
-  return <h1>I Like {fav} </h1>;
+  return <div>
+      <h2>I Like {name}</h2>
+      <img src={picture}/>
+  </div>
 }
+
+const foodILike = [
+  {
+    name: "kimchi",
+    image: "http://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
+  },
+  {
+    name: "Kimbap",
+    image: "https://mblogthumb-phinf.pstatic.net/MjAxODAyMDNfNTMg/MDAxNTE3NjU4OTc5MjQ1.ZKPcsvt3m0aSDIZMzRvIFJCsOGcM0pdPmUmoeDgeLkMg.Xma30vr2wbDhIa8J8276fsWKB0elqmcoaUdHk62k8IQg.JPEG.yuki5619/P1420687.JPG?type=w2"
+  }
+];
 
 function App() {
   return(
    <div>
-      Hello!!
       <h1>OHHHHHHHH</h1> 
-      <Food fav="kimchi" />  
-      <Food fav="ramen" />  
-      <Food fav="samgiopsal" />  
-      <Food fav="chuk" />  
+      {foodILike.map(dish =>(
+          <Food name={dish.name} picture={dish.image}/>
+      ))}
     </div>
   ); 
 }
