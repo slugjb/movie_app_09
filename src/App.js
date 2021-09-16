50d704adceffaa9159e0994ff8ee1fb80df4d375
Junbae Park <slug_jb@naver.com>
2021-09-16 13:11:04 +0900

    0915_v02

--- src/App.js
+++ src/App.js
@@ -1,33 +1,28 @@
-import React from "react";
+import React from 'react';
+import PropTypes from "prop-types";
 
-function Food({name, picture})
-{
-  return <div>
-      <h2>I Like {name}</h2>
-      <img src={picture}/>
-  </div>
-}
+class App extends React.Component{
 
-const foodILike = [
-  {
-    name: "kimchi",
-    image: "http://health.chosun.com/site/data/img_dir/2020/07/21/2020072103114_0.jpg"
-  },
-  {
-    name: "Kimbap",
-    image: "https://mblogthumb-phinf.pstatic.net/MjAxODAyMDNfNTMg/MDAxNTE3NjU4OTc5MjQ1.ZKPcsvt3m0aSDIZMzRvIFJCsOGcM0pdPmUmoeDgeLkMg.Xma30vr2wbDhIa8J8276fsWKB0elqmcoaUdHk62k8IQg.JPEG.yuki5619/P1420687.JPG?type=w2"
+  state = {
+      count: 0
+  };
+
+  add = () => {
+    this.setState({count: this.state.count + 1});
+  };
+  minus = () => {
+    this.setState({count: this.state.count - 1});
+  };
+
+  render (){
+    return( 
+      <div>
+        <h1>the number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
   }
-];
-
-function App() {
-  return(
-   <div>
-      <h1>OHHHHHHHH</h1> 
-      {foodILike.map(dish =>(
-          <Food name={dish.name} picture={dish.image}/>
-      ))}
-    </div>
-  ); 
 }
 
 export default App;
